b283364df748e6849353184ff65f1276c7b5d354
itayozer9 <itayozer9@gmail.com>
2024-06-19 18:11:50 +0300

    init

--- putData.py
+++ putData.py
@@ -94,11 +94,11 @@ def main(symbols, option_date, percentage):
         for row in results:
             writer.writerow([
                 row['ticker'], 
-                f"{row['last_close']:.2f}" if not row['last_close'].is_integer() else int(row['last_close']), 
-                f"{row['strike_price']:.2f}" if not row['strike_price'].is_integer() else int(row['strike_price']), 
-                f"{row['bid_price']:.2f}" if not row['bid_price'].is_integer() else int(row['bid_price']),
-                f"{row['collateral']:.2f}" if not row['collateral'].is_integer() else int(row['collateral']),
-                f"{row['yield']:.2f}" if not row['yield'].is_integer() else int(row['yield'])
+                f"{row['last_close']:.2f}" if not float(row['last_close']).is_integer() else int(row['last_close']), 
+                f"{row['strike_price']:.2f}" if not float(row['strike_price']).is_integer() else int(row['strike_price']), 
+                f"{row['bid_price']:.2f}" if not float(row['bid_price']).is_integer() else int(row['bid_price']),
+                f"{row['collateral']:.2f}" if not float(row['collateral']).is_integer() else int(row['collateral']),
+                f"{row['yield']:.2f}" if not float(row['yield']).is_integer() else int(row['yield'])
             ])
     
     print(f"Data written to {csv_file}")
